ea7449074f9fcad0b815877b0de844dc1a4071ca
joshbrown31 <joshbrown31@gmail.com>
2022-03-07 15:55:24 -0500

    commit

--- fundamentals/fundamentals/For_loop_basic_I/for_loop_basic_I.py
+++ fundamentals/fundamentals/For_loop_basic_I/for_loop_basic_I.py
@@ -11,3 +11,50 @@ Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the fin
 Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
 
 Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines) """
+
+#Basic
+
+from cgi import print_arguments
+
+
+for x in range(151):
+    print(x)
+
+#Multiples of 5
+
+for x in range(0, 1001, 5):
+    print(x)
+
+#Counting the Dojo Way
+
+for x in range(1, 101):
+    if x % 10 == 0:
+        print("Coding Dojo")
+    elif x % 5 == 0:
+        print("Coding")
+    else:
+        print(x)
+
+#Whoa, That Number's Huge
+
+y = 0
+
+for x in range(0, 500000):
+    if x % 2 != 0:
+        y += x
+
+print(y)
+
+#Countdown by fours
+
+for x in range(2018, 0, -4):
+    if x > 0:
+        print(x)
+
+#Flexible Counter
+lowNum = 1
+highNum = 9
+mult = 3
+for x in range (lowNum, highNum):
+    if x % mult == 0:
+        print(x)
