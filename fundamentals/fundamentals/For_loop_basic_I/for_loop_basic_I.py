2f8f4d144b8a2000369d626ce7848afa45d89539
joshbrown31 <joshbrown31@gmail.com>
2022-03-07 15:29:30 -0500

    commit

--- fundamentals/fundamentals/For_loop_basic_I/for_loop_basic_I.py
+++ fundamentals/fundamentals/For_loop_basic_I/for_loop_basic_I.py
@@ -11,4 +11,3 @@ Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the fin
 Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
 
 Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines) """
-
