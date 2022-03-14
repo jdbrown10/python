632afce6cde7d956414b0876cd9dc499c6137645
joshbrown31 <joshbrown31@gmail.com>
2022-03-13 20:14:28 -0400

    commit

---
+++ flask/Counter/server.py
@@ -0,0 +1,36 @@
+from itertools import count
+from flask import Flask, render_template, redirect, request, session
+app = Flask (__name__)
+app.secret_key = "sasadffsadsa4677d5fasdf78sa6f"
+
+
+#==========================
+#ROOT ROUTE - Renders Form
+#==========================
+@app.route("/")
+def index():
+    if 'counter' in session:
+        session['counter'] += 1
+    else:
+        session['counter'] = 1
+    return render_template("index.html")
+
+#==========================
+#ADD ROUTE - Adds to Count
+#==========================
+
+@app.route("/add")
+def add():
+    session['counter'] += 1
+    return redirect("/")
+
+#==========================
+#RESET ROUTE - Resets Count
+#==========================
+@app.route("/destroy_session")
+def reset():
+    session.clear()
+    return redirect("/")
+
+if __name__=="__main__":
+    app.run(debug=True, port=5001)
